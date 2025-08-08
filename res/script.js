7c84d8f264c99521f436c6b060cb0e1649771ff1
Dory <pemegomachado@gmail.com>
2025-08-08 20:38:40 +0200

    Made encode_tsf() compliant with v1.1

--- res/script.js
+++ res/script.js
@@ -44,14 +44,17 @@ function encode_tsf(into, image_url, options = {
     const processArray = (arr) =>
         !arr?.length ? ["0", ""] : ["1", arr.map(({content, value}) => `${content}|${value}`).join(",")];
 
+    let number = 0;
+    number = number & (Number(options.big) << 0);
+    number = number & (Number(options.small) << 1);
+    number = number & (Number(options.hush) << 2);
+    number = number & (Number(options.backwards) << 3);
+
     // Generate arrays and make it into the proper data to return
     return ["15",
             into,
             image_url,
-            Number(options.big),
-            Number(options.small),
-            Number(options.hush),
-            Number(options.backwards),
+            number,
             options.stutter.toString(),
             options.proxy_prefix ?? "",
             options.proxy_suffix ?? "",
